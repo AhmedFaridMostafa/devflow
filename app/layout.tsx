d72cbf8e13c294d22a7e7b218bf37a0df8476b0a
Ahmed Fared <ahmed.super997@gmail.com>
2026-01-13 05:22:47 +0200

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/context/theme-provider";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -13,7 +15,6 @@ const spaceGrotesk = localFont({
   variable: "--font-space-grotesk",
   weight: "300 400 500 700",
 });
-
 
 export const metadata: Metadata = {
   title: "DevFlow",
@@ -30,11 +31,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-      className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
