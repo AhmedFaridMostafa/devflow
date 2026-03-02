e45f73a387e47f7de464114c4bdfcc13ab3214ec
Ahmed Fared <ahmed.super997@gmail.com>
2026-03-02 05:27:34 +0200

    Display Tag Specific Questions

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -16,9 +16,6 @@ const Tags = async ({ searchParams }: RouteParams) => {
     });
 
 
-    console.log(result)
-
-
     return (
         <>
             <h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
